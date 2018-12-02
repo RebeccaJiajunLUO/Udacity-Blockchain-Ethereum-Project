3f05077ff1e7ee2c4eff1ac01d38628f50c3db8d
Chuan <callforsky@gmail.com>
2018-12-02 09:20:21 -0500

    Going back to the origin point
    At this commit, the tests passed

--- smart_contracts/contracts/StarNotary.sol
+++ smart_contracts/contracts/StarNotary.sol
@@ -5,19 +5,13 @@ import 'openzeppelin-solidity/contracts/token/ERC721/ERC721.sol';
 contract StarNotary is ERC721 { 
 
     struct Star { 
-        string name;
-        string starStory;
-        string ra;
-        string dec;
-        string mag;
+        string name; 
     }
 
     mapping(uint256 => Star) public tokenIdToStarInfo; 
     mapping(uint256 => uint256) public starsForSale;
 
     function createStar(string _name, uint256 _tokenId) public { 
-        // Create a 'Star memory newStar' variable
-        // Verify uniquenessFactor 'keccak256(_ra, _dec, _mag)'
         Star memory newStar = Star(_name);
 
         tokenIdToStarInfo[_tokenId] = newStar;
@@ -26,27 +20,23 @@ contract StarNotary is ERC721 {
     }
 
     function putStarUpForSale(uint256 _tokenId, uint256 _price) public { 
-        // 'require' owner in the token is equal to 'msg.sender'
         require(this.ownerOf(_tokenId) == msg.sender);
 
         starsForSale[_tokenId] = _price;
     }
 
     function buyStar(uint256 _tokenId) public payable { 
-        // verify if the star is for sale
-        // verify if you have any amount
         require(starsForSale[_tokenId] > 0);
-        
+
         uint256 starCost = starsForSale[_tokenId];
         address starOwner = this.ownerOf(_tokenId);
         require(msg.value >= starCost);
 
         _removeTokenFrom(starOwner, _tokenId);
         _addTokenTo(msg.sender, _tokenId);
-        
+
         starOwner.transfer(starCost);
 
-        // check if everything is okay to transfer
         if(msg.value > starCost) { 
             msg.sender.transfer(msg.value - starCost);
         }
