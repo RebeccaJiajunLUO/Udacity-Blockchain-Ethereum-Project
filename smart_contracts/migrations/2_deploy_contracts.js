801a5437d1919de1c2b9b8958618edcd9079a82a
Chuan <callforsky@gmail.com>
2018-12-07 15:53:50 -0500

    Rough Draft

--- smart_contracts/migrations/2_deploy_contracts.js
+++ smart_contracts/migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
 var starNotary = artifacts.require("./starNotary.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(starNotary);
+module.exports = function(deployer, network, accounts) {
+  if (network == "rinkeby") {
+    deployer.deploy(starNotary, {from: accounts[0]});
+  } else if (network == "development") {
+    deployer.deploy(starNotary, {from: accounts[0]});
+  }
 };
